333885d4e43942d5d1cd6e43cf94a19364c20281
ahmose231 <36313066+ahmose231@users.noreply.github.com>
2024-12-24 17:51:42 +0300

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LEN 64
+
+int main(void) {
+	// your code goes here
+	
+	int c;
+	char word[LEN];
+	int index=0;
+	
+	long long int total=0;
+	
+	c=fgetc(stdin);
+	while(c!=EOF)
+	{
+		int borked=0;
+		if(c=='m')
+		{
+			index=0;
+			int part=1;
+			word[index++]=c;
+			
+			c=fgetc(stdin);
+			if(c=='u')
+			{
+				word[index++]=c;
+				c=fgetc(stdin);
+				if(c=='l')
+				{
+					word[index++]=c;
+					c=fgetc(stdin);
+					if(c=='(')
+					{
+						word[index++]=c;
+						do
+						{
+							c=fgetc(stdin);
+							
+							if(!(c<='9'&&c>='0'))
+							{
+								if(!((c==',' && part==1) || (c==')' && part==2)))
+								{
+									borked=1;
+									break;
+								}
+									
+								else
+									part++;
+							}
+							word[index++]=c;
+						} while(c!=')');
+						word[index++]=c;
+						if(word[index-2]==',') borked=1;
+						if(part==1) borked=1;
+						
+						word[index]=0;
+						
+						int a,b;
+						if(borked==0 && sscanf(word, "mul(%d,%d)", &a, &b)==2)
+						{
+							total += a*b;
+						}
+						
+					}
+				}
+			}
+		}
+		c=fgetc(stdin);
+	}
+	
+	printf("%lld\n",total);
+	return 0;
+}
